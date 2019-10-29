7a4298b96b60f41a93073dcf75612ac40549efee
zhangmanyi <will.up2018@foxmail.com>
2019-10-29 11:31:54 +0800

    10/29-commit

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,20 @@ module.exports = {
   devServer: {
     port: 9527,
     // open: true
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://www.example.org',
+    //     changeOrigin: true,
+    //     ws: true,
+    //     pathRewrite: {
+    //       '^/api': ''
+    //     }
+    //   }
+    // }
   },
+  // configureWebpack: config => {
+
+  // }
   // chainWebpack: (config) => {
   //   //set第一个参数：设置的别名，第二个参数：设置的路径
   //   config.resolve.alias.set('@', path.join(__dirname, './src'))
